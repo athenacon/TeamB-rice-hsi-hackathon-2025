3e1cd41b9910f3c0c85447a62eee7e8d121c1ae6
nonbinary-duck <46252182+nonbinary-duck@users.noreply.github.com>
2025-05-07 16:26:23 +0100

    Updated the SLURM script to be more tollerant of other tenants

--- hackathon_run.sh
+++ hackathon_run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=256G
+#SBATCH --mem=64G
 #SBATCH -o slurm.%j.out
 #SBATCH -e slurm.%j.err
 #SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:3
+#SBATCH --gres=gpu:1
 #SBATCH --partition=a100_full
-#SBATCH --nodelist=agpu002
 
 #SBATCH --mail-type=ALL 
-#SBATCH --mail-user=YOURMAIL
+#SBATCH --mail-user=your@email.com
+#SBATCH --time=03:00:00
 
 # ADD DEBUGGING ECHO COMMANDS HERE
 echo "Starting job at $(date)" 
